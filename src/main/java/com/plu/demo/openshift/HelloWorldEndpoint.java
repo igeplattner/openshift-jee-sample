37aab75a4da6efcd7ed50166278f325d4bb76ef8
Urs Plattner <urs.plattner@ipi.ch>
2020-03-06 13:12:38 +0100

    PLU-3: test

--- src/main/java/com/plu/demo/openshift/HelloWorldEndpoint.java
+++ src/main/java/com/plu/demo/openshift/HelloWorldEndpoint.java
@@ -1,17 +1,16 @@
 package com.plu.demo.openshift;
 
-
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloWorldEndpoint {
-    @GET
-    @Produces("text/plain")
-    public Response doGet() {
-        return Response.ok("Hello from Thorntail!").build();
-    }
+
+  @GET
+  @Produces("text/plain")
+  public Response doGet() {
+    return Response.ok("Hello from Openshift!").build();
+  }
 }
